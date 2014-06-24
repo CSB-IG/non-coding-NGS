b64401cdaafb2bbbb16c51551644b38dbff5d217
absorto <rgarcia@inmegen.gob.mx>
2014-06-24 17:58:26 -0500

    unnecesary substring "chr"

--- extract_bed.py
+++ extract_bed.py
@@ -13,7 +13,7 @@ tsv = [v.strip().split() for v in args.tsv.readlines()]
 
 for v in tsv:
     for n in bed:
-        if v[0] == "chr%s" % n[0] and int(v[1]) >= int(n[1]) and int(v[2]) <= int(n[2]):
+        if v[0] == n[0] and int(v[1]) >= int(n[1]) and int(v[2]) <= int(n[2]):
             print " ".join(v)
             break
 
